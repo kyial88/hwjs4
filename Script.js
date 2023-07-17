cca1e7b5ea8e1564187915eb7b90f7d37b0a6c15
kyial88 <klibraeva@gmail.com>
2023-07-17 09:54:26 +0600

    poka ne proveryaite pls

---
+++ Script.js
@@ -0,0 +1,14 @@
+// ['lorem', 'ipsum', 'dolor'] => ['Lorem', 'ipsum', 'Dolor']
+// let array =['lorem', 'ipsum', 'dolor']
+// console.log(array.map((el,   idx) => idx === 2
+//     ? el[0].toUpperCase()+el.slice(1): el ))
+
+// ['lorem', 'ipsum', 'dolor'] => ['LOREM', 'IPSUM', 'DOLOR']
+let array = ['lorem', 'ipsum', 'dolor']
+let el = 0
+console.log(array.map((el,   idx) => idx < array.length ? el[0].toUpperCase()+el.slice(0): el ))
+
+
+
+
+
