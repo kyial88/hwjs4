b01a4e920ac9e45a9102a85ff2f1303617e1b29c
kyial88 <klibraeva@gmail.com>
2023-07-17 13:03:32 +0600

    proect 4

--- Script.js
+++ Script.js
@@ -4,11 +4,14 @@
 //     ? el[0].toUpperCase()+el.slice(1): el ))
 
 // ['lorem', 'ipsum', 'dolor'] => ['LOREM', 'IPSUM', 'DOLOR']
-let array = ['lorem', 'ipsum', 'dolor']
-let el = 0
-console.log(array.map((el,   idx) => idx < array.length ? el[0].toUpperCase()+el.slice(0): el ))
+// let array = ['lorem', 'ipsum', 'dolor']
+// let el = 0
+// console.log(array.map((el,   idx) => idx < array.length ? el[0].toUpperCase()+el.slice(0): el ))
 
+// ['lorem', 'ipsum', 'dolor', 'sit', 'amet'] => 'lorem-ipsum-dolor-sit-amet'
 
+let array = ['lorem', 'ipsum', 'dolor', 'sit', 'amet']
+console.log(array.map(el =>el.trim()).join("-"))
 
 
 
